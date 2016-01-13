fbfec232b89ff525ceef4469c2d16ad5afab1cca
Matt Turi <mturi@mort11.org>
2016-01-12 20:27:57 -0500

    Fixed package locations

--- src/org/mort11/subsystems/RightDT.java
+++ src/org/mort11/subsystems/RightDT.java
@@ -1,18 +1,10 @@
-package org.usfirst.frc.team11.robot.subsystems;
+package org.mort11.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
 public class RightDT extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
     }
 }
 
