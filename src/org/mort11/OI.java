9bf9b4d893129c38d430347e59beb2aacae41004
Jakob Shortell <jshortell@mort11.org>
2016-01-13 19:45:47 -0500

    really sketchy OI code it should work for now I will fix it later.

--- src/org/mort11/OI.java
+++ src/org/mort11/OI.java
@@ -1,6 +1,30 @@
 package org.mort11;
 
-public class OI {
 
+
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+	
+	
+	 Joystick left = new Joystick(0);
+	    Joystick right = new Joystick(1);
+	    
+	public double getLeftJoy() {
+        return doThreshold(-left.getY());
+    }
+
+    public double getRightJoy() {
+        return doThreshold(-right.getY());
+    }
+    public static double doThreshold(double input) {
+        if (Math.abs(input) <= 0.05) {
+            return 0;
+        }
+            return input / Math.abs(input)
+                    * (Math.abs(input) - 0.05)
+                    / (1 - 0.05);
+        }
 }
 
