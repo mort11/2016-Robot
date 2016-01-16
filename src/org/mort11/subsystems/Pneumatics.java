61fc97906a556275fb8186b403c7de84856c594a
Seven <seven.kurt@motsd.org>
2016-01-16 09:42:39 -0500

    created pneumatics

--- src/org/mort11/subsystems/Pneumatics.java
+++ src/org/mort11/subsystems/Pneumatics.java
@@ -1,6 +1,33 @@
 package org.mort11.subsystems;
 
-public class Pneumatics {
-				
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-}
+
+public class Pneumatics {
+	DoubleSolenoid sol;
+	 boolean engaged; 
+
+	    public Pneumatics(int engagedPort, int notEngagedPort) {
+	        sol = new DoubleSolenoid(engagedPort, notEngagedPort);
+	        engaged = false;
+	    }
+
+
+	    public void initDefaultCommand() {
+	    }
+
+	    public void setSolenoid(boolean engage) {
+	        if (engage) {
+	            sol.set(DoubleSolenoid.Value.kForward); 
+	        } else {
+	            sol.set(DoubleSolenoid.Value.kReverse); 
+	        }
+	        engaged = engage;
+	        System.out.println("state 2: " + engage);
+	    }
+
+	    public boolean isEngaged() {
+	        return engaged;
+	    }
+	}
+	
