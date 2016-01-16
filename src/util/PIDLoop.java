af93b921929b00d052d1fa70dc4832a70aeb143d
sahit <chsahit@gmail.com>
2016-01-16 12:09:25 -0500

    added profiling

--- src/util/PIDLoop.java
+++ src/util/PIDLoop.java
@@ -10,13 +10,15 @@ public class PIDLoop {
 							//when we can start the timer
 	double currTime = 0,oldTime = 0;
 	Timer timer = new Timer();
+	double vel_max = 3;
 	public PIDLoop(double target,double kP, double kI) {
 		this.target = target;
 		this.kP = kP;
 		this.kI = kI;
 	}
 	
-	public double getOutput(double  pos){
+	
+	public double getOutput_notStaggered(double  pos){
 		double error = target - pos;
 		//shift to I
 		if (Math.abs(error/pos) < 0.2){
@@ -37,5 +39,27 @@ public class PIDLoop {
 			return error * kP;
 		}
 	}
+	
+	public double getOutput(double pos) {
+		if(!isNear) {
+			timer.start();
+			isNear = true;
+		}
+		currTime = timer.get();
+		double error = getLocation(currTime,pos) - pos;
+		double deltaT = currTime - oldTime;
+		netError += error * deltaT;
+		oldTime = currTime;
+		return (error * kP + netError * kI);
+	}
+	
+	public double getLocation(double time,double pos) {
+		if(time > pos/vel_max) {
+			return pos;
+		}
+		return vel_max * time;
+	}
+	
+	
 
 }
