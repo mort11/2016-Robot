fc178d21a76e2128cd4c56426d78e1af7047ec24
Michael Kozak <Michael Kozak>
2016-01-21 20:51:56 -0500

    Fixed intakeRollers

--- src/org/mort11/util/TeleopConstants.java
+++ src/org/mort11/util/TeleopConstants.java
@@ -5,5 +5,7 @@ public class TeleopConstants {
 	 public static final int RIGHT_JOYSTICK = 2;
 	 public static final int EE_JOYSTICK = 3;
 	 public static final int PISTON_BUTTON = 1;
+	 public static final int INTAKE_BUTTON = 4;
+	 public static final int OUTTAKE_BUTTON = 5;
 
 }
