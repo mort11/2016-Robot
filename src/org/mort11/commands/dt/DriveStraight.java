3ab83efba682792a6badc73c3943c1f83487ca60
koosk <matthew.krzyzanowski@gmail.com>
2016-01-21 20:16:36 -0500

    remov3d timer from DriveSgtraggggbb hubvb uh class

--- src/org/mort11/commands/dt/DriveStraight.java
+++ src/org/mort11/commands/dt/DriveStraight.java
@@ -1,6 +1,5 @@
 package org.mort11.commands.dt;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.mort11.Robot;
@@ -12,7 +11,6 @@ public class DriveStraight extends Command {
 	private double driveDistance;
 	private DTSide left = Robot.leftSide;
 	private DTSide right = Robot.rightSide;
-	private Timer timer;
 	private PIDLoop pd;
 	private double curDist;
 	private double speed;
@@ -21,17 +19,15 @@ public class DriveStraight extends Command {
        this.driveDistance = distance;
        requires(left);
        requires(right);
-       timer = new Timer();
        pd = new PIDLoop(driveDistance, .01, 0); //placeholder values, must test
     }
 
     protected void initialize() {
     	DTSide.resetEnc();
-    	timer.start();
     }
 
     protected void execute() {
-        curDist = DTSide.getDist();
+        curDist = DTSide.getDist(); 
         speed = pd.getOutput(curDist);
     	DTSide.setSpeed(speed);
     	SmartDashboard.putNumber("Distance Traveled", DTSide.getDist()); //gets and displays distance traveled
