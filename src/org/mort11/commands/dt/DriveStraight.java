ea16c26fec839d4b20d3360f1a819a4c22e27057
koosk <matthew.krzyzanowski@gmail.com>
2016-01-22 19:36:22 -0500

    saved the world --turi

---
+++ src/org/mort11/commands/dt/DriveStraight.java
@@ -0,0 +1,57 @@
+package org.mort11.commands.dt;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.mort11.Robot;
+import org.mort11.subsystems.dt.DTSide;
+import org.mort11.util.PIDLoop;
+
+public class DriveStraight extends Command {
+
+	private double driveDistance = 5;
+	private DTSide left = Robot.leftSide;
+	private DTSide right = Robot.rightSide;
+	private Timer timer;
+	private PIDLoop pd;
+	private double curDist;
+	private double speed;
+	
+    public DriveStraight() {
+       requires(left);
+       requires(right);
+       timer = new Timer();
+       pd = new PIDLoop(driveDistance, .01, 0); //placeholder values, must test
+    }
+
+    protected void initialize() {
+    	DTSide.resetEnc();
+    	timer.start();
+    }
+
+    protected void execute() {
+        curDist = DTSide.getDist();
+        speed = pd.getOutput(curDist);
+    	DTSide.setSpeed(speed);
+    	SmartDashboard.putNumber("Distance Traveled", DTSide.getDist()); //gets and displays distance traveled
+        SmartDashboard.putNumber("PWM Value", DTSide.getSpeed()); //get most recently set PWM value, between -1.0 and 1.0
+    	
+    }
+
+    protected boolean isFinished() {
+        return this.inTresh();
+    }
+
+    protected void end() {
+    	DTSide.resetEnc();
+    }
+
+    protected void interrupted() {
+    }
+    
+    protected boolean inTresh(){
+        //placeholder values, must test
+        return speed < .01 && speed > -.01;
+
+    }
+}
