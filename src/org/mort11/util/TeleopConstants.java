c53cd211fcd9ff6aca763867f964899b9267d22d
rotoole <ryan.otoole@motsd.org>
2016-01-26 19:07:09 -0500

    Made some changes to ShootingMechanism

--- src/org/mort11/util/TeleopConstants.java
+++ src/org/mort11/util/TeleopConstants.java
@@ -5,5 +5,6 @@ public class TeleopConstants {
 	 public static final int RIGHT_JOYSTICK = 2;
 	 public static final int EE_JOYSTICK = 3;
 	 public static final int PISTON_BUTTON = 1;
+	 public static final int JOYSTICK_BUTTON1 = 2;
 
 }
