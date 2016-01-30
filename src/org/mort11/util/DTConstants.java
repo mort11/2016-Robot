81db591e2bdd41f609eb1231382a99d62b67d1e2
koosk <matthew.krzyzanowski@gmail.com>
2016-01-29 19:28:45 -0500

    added correct tick count for encoders, <3 matt

--- src/org/mort11/util/DTConstants.java
+++ src/org/mort11/util/DTConstants.java
@@ -4,6 +4,6 @@ public class DTConstants {
 		//copypasta from 2015
 	 	public static final double DT_P = 0.3;
 	    public static final double WHEEL_RADIUS_INCHES = 4; 
-	    public static final int ENC_CPR = 256;
+	    public static final int ENC_CPR = 128; //tick count from kop robot
 	    public static final double INCHES_PER_PULSE = 4 * Math.PI / ENC_CPR;
 }
