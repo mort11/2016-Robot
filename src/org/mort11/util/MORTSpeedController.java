848446226fd98d0a230ed904016dfa3d304db89e
Matt Turi <mturi@mort11.org>
2016-02-03 22:51:48 -0500

    Created MORTCANTalon and provided sufficient documentation to accompany it

--- src/org/mort11/util/MORTSpeedController.java
+++
@@ -1,9 +0,0 @@
-package org.mort11.util;
-
-/**
- * MORTSpeedController - Wrapped implementation of SpeedController
- *
- * @author Matt Turi <mturi@mort11.org>
- */
-public class MORTSpeedController {
-}
