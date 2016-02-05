b89b3998c822af0319c003b6e406eebe4b3aca02
Matt Turi <mturi@mort11.org>
2016-02-04 20:20:45 -0500

    Fixed unused variables in robot

--- src/org/mort11/Robot.java
+++ src/org/mort11/Robot.java
@@ -1,17 +1,15 @@
 package org.mort11;
 
-import java.sql.Timestamp;
-import java.util.Date;
-
-import org.mort11.commands.auton.DriveArc;
-import org.mort11.commands.auton.DriveStraight;
-import org.mort11.util.Logger;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.mort11.commands.auton.DriveArc;
+import org.mort11.commands.auton.DriveStraight;
+import org.mort11.commands.auton.WaitTime;
+import org.mort11.util.Logger;
 
 /**
  * Robot - Main Robot class
@@ -28,31 +26,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
  */
 public class Robot extends IterativeRobot {
     public static OI oi;
+    public static HardwareAdaptor adaptor = new HardwareAdaptor();
+
     Command driveArc;
     Command autonomousCommand;
     SendableChooser autonomousChooser;
-    public static HardwareAdaptor adaptor = new HardwareAdaptor();
-    Date date;
-//    Command autonomousCommand;
-//    SendableChooser autonomousChooser;
 
     @Override
     public void robotInit() {
 
         driveArc = new DriveArc(12 * Math.PI, 0.5 * Math.PI);
-    	//driveArc = new DriveStraight(100);
-        date = new Date();
 
         Logger.init("/home/lvuser/test");
 
         oi = new OI();
 
         // Have operator choose autonomous mode
-//        autonomousChooser = new SendableChooser();
-//        autonomousChooser.addDefault("Do Nothing for 10s", new WaitTime(10));
-//        autonomousChooser.addObject("Drive Straight [20in.]", new DriveStraight(20));
-//        autonomousChooser.addObject("Drive Arc [Unknown units]", new DriveArc(1.33 * Math.PI, 0.5 * Math.PI));
-//        SmartDashboard.putData("Autonomous Mode", autonomousChooser);
+        autonomousChooser = new SendableChooser();
+        autonomousChooser.addDefault("Do Nothing for 10s", new WaitTime(10));
+        autonomousChooser.addObject("Drive Straight [20in.]", new DriveStraight(20));
+        autonomousChooser.addObject("Drive Arc [Unknown units]", new DriveArc(1.33 * Math.PI, 0.5 * Math.PI));
+        SmartDashboard.putData("Autonomous Mode", autonomousChooser);
     }
 
     @Override
@@ -61,9 +55,8 @@ public class Robot extends IterativeRobot {
     }
 
     public void autonomousInit() {
-        System.out.println("auton initting");
-         driveArc.start();
-        //DriveStraight.start();
+        System.out.println("STARTING AUTONOMOUS");
+        driveArc.start();
     }
 
     @Override
@@ -73,7 +66,7 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopInit() {
-//        if (autonomousCommand != null) autonomousCommand.cancel();
+        if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
     @Override
