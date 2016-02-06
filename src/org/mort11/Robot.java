3103049756976c30d8347646e0cdb6cd5947ed59
koosk <matthew.krzyzanowski@gmail.com>
2016-02-05 20:33:19 -0500

    fixed date error within robot.java and added navx libs into buildproperties

--- src/org/mort11/Robot.java
+++ src/org/mort11/Robot.java
@@ -38,7 +38,6 @@ public class Robot extends IterativeRobot {
 
         //driveArc = new DriveArc(18 * Math.PI, 0.5 * Math.PI);
     	driveArc = new DriveStraight(200);
-        date = new Date();
 
         Logger.init("/home/lvuser/test");
 
