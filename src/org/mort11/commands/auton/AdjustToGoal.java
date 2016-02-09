12a9f0852bb80c436c6932f4cb502b32ae5ef1e5
Matt Turi <mturi@mort11.org>
2016-02-08 19:03:38 -0500

    Added NavX libs and started refactoring code

--- src/org/mort11/commands/auton/AdjustToGoal.java
+++ src/org/mort11/commands/auton/AdjustToGoal.java
@@ -1,51 +1,54 @@
 package org.mort11.commands.auton;
 
-import org.mort11.Robot;
-import org.mort11.sensors.SensorDealer;
-
 import edu.wpi.first.wpilibj.command.Command;
+import org.mort11.Robot;
+import org.mort11.subsystems.Camera;
+import org.mort11.subsystems.dt.DTSide;
 
 /**
+ * AdjustToGoal - Adjust robot to goal
  *
+ * @author Sahit Chintalapudi <schintalapudi@mort11.org>
  */
 public class AdjustToGoal extends Command {
-	boolean finished = false;
+    boolean finished = false;
+    private DTSide left = Robot.adaptor.leftSide;
+    private DTSide right = Robot.adaptor.rightSide;
+    private Camera camera = Robot.adaptor.cam;
+
     public AdjustToGoal() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.adaptor.leftSide);
-    	requires(Robot.adaptor.rightSide);
-    	requires(Robot.adaptor.cam);
+        requires(left);
+        requires(right);
+        requires(camera);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
-    	//SensorDealer.getInstance().getAHRS().zeroYaw();
+        //SensorDealer.getInstance().getAHRS().zeroYaw();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.adaptor.cam.setPicture();
-    	System.out.println("looping");
-		double x_val =Robot.adaptor.cam.getX();
-		if(x_val == -1){
-			System.out.println("not found");
-			finished = true;
-		}
-		System.out.println("Centering");
-		if(x_val < 135) {
-			Robot.adaptor.leftSide.set(-0.15);
-			Robot.adaptor.rightSide.set(0.15);
-			System.out.println("too far right");
-		} else if(x_val > 185) {
-			Robot.adaptor.leftSide.set(0.15);
-			Robot.adaptor.rightSide.set(-0.15);
-			System.out.println("too far left");
-		} else {
-			System.out.println("centered");
-			finished = true;
-		}
-		Robot.adaptor.cam.setPicture();
+        Robot.adaptor.cam.setPicture();
+        System.out.println("looping");
+        double x_val = Robot.adaptor.cam.getX();
+        if (x_val == -1) {
+            System.out.println("not found");
+            finished = true;
+        }
+        System.out.println("Centering");
+        if (x_val < 135) {
+            Robot.adaptor.leftSide.set(-0.15);
+            Robot.adaptor.rightSide.set(0.15);
+            System.out.println("too far right");
+        } else if (x_val > 185) {
+            Robot.adaptor.leftSide.set(0.15);
+            Robot.adaptor.rightSide.set(-0.15);
+            System.out.println("too far left");
+        } else {
+            System.out.println("centered");
+            finished = true;
+        }
+        Robot.adaptor.cam.setPicture();
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/mort11/subsystems/Camera.java
+++ src/org/mort11/subsystems/Camera.java
@@ -1,21 +1,18 @@
 package org.mort11.subsystems;
 
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.Range;
+import com.ni.vision.NIVision.RawData;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-import com.ni.vision.NIVision; // "..."
-import com.ni.vision.NIVision.Image; // "..."
-import com.ni.vision.NIVision.RawData; // "..."
-import com.ni.vision.NIVision.Range; // "..."
-
-import edu.wpi.first.wpilibj.CameraServer; // "..."
-
 /**
- * This class takes a picture, finds the location of the largest object of a pretested 
+ * This class takes a picture, finds the location of the largest object of a pretested
  * color in terms of X and Y, and also identifies its size. These variables can also be
  * returned, and will be printed to the Riolog.
  */
 public class Camera extends Subsystem {
-	
 	int session; // declares session variable
 	Image frame, thresh, filter; // declares image variables
 	RawData imageData; // declares raw data variable
@@ -23,7 +20,7 @@ public class Camera extends Subsystem {
 	int num_particles, largest_particle, largest_particle2; // declares integer variables
 	int i, j; // declares integer variables
 	double particle_size, largest_size, Location_X;
-	
+
 	//takes a picture of the area and identifies the x/y location of the largest particle
 	public void setPicture() {
 		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0); // assigns new RGB Image Value
@@ -33,8 +30,8 @@ public class Camera extends Subsystem {
 		/**
 		 * Open session at camera name assigned
 		 */
-		session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController); 
-	
+		session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+
 		NIVision.IMAQdxConfigureGrab(session); // configures grab for set session
 
 		/**
@@ -45,19 +42,19 @@ public class Camera extends Subsystem {
 		rangeV = new Range(220, 255);
 
 		imageData = new RawData(); // creates new raw data object
-		
+
 		i = 0; // assigns integer value
 		largest_particle = 0; // assigns integer value
 		largest_particle2 = 0; // assigns integer value
 		particle_size = 0; // assigns double value
 		largest_size = 0; // assigns double value
-		
+
 		NIVision.IMAQdxStartAcquisition(session); // starts aquisition for set session
-		
+
 		NIVision.IMAQdxGrab(session, frame, 1); // grabs image taken by camera for editing
 
 		CameraServer.getInstance().setImage(frame); // gets instance in order to manually adjust image
-		
+
 		/**
 		 * Enact color threshold on image
 		 */
@@ -65,7 +62,7 @@ public class Camera extends Subsystem {
 
 		num_particles = NIVision.imaqCountParticles(thresh, 0); // assigns value equal to particle number
 		//System.out.println("num_particles " + num_particles); // prints num_particles value
-		
+
 		largest_particle = 0;
 		largest_size = 0;
 		Location_X = -1;
@@ -84,7 +81,7 @@ public class Camera extends Subsystem {
 				if (particle_size > largest_size) {
 					largest_size = particle_size;
 					largest_particle = k;
-					
+
 				}
 			}
 
@@ -94,31 +91,31 @@ public class Camera extends Subsystem {
 			 * Measure tote location
 			 */
 			Location_X = NIVision.imaqMeasureParticle(thresh, largest_particle, 0, NIVision.MeasurementType.MT_CENTER_OF_MASS_X);
-			System.out.println("location X " + Location_X); // prints final location to riolog			
+			System.out.println("location X " + Location_X); // prints final location to riolog
 		}
-		
+
 		i++; // increases value of i by one
-		
+
 		NIVision.IMAQdxStopAcquisition(session); // stops aquisition for set session
 		frame.free();
 		thresh.free();
 	}
-	
+
 	public double getX() {
 		return Location_X;
 	}
-	
+
 	public double getSize() {
 		return particle_size;
 	}
-	
-	
-    // Put methods for controlling this subsystem
+
+
+	// Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
     }
-    
-    
+
+
 }
