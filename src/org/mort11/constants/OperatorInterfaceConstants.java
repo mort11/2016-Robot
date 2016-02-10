fbc6235b41306b87c89f07c48c68992a1700b49e
Matt Turi <mturi@mort11.org>
2016-02-09 21:56:18 -0500

    Changed button id shifter

--- src/org/mort11/constants/OperatorInterfaceConstants.java
+++ src/org/mort11/constants/OperatorInterfaceConstants.java
@@ -9,12 +9,15 @@ package org.mort11.constants;
  * @author Ryan Thant <ryanthant1@gmail.com>
  */
 public class OperatorInterfaceConstants {
+    // USB
     public static final int LEFT_JOYSTICK = 0; // Temp values for testing with one joystick
     public static final int RIGHT_JOYSTICK = 1; // Temp values for testing with one joystick
     public static final int EE_JOYSTICK = 2;
+
+    // Joystick Buttons
     public static final int PISTON_BUTTON = 1;
     public static final int INTAKE_BUTTON = 4;
     public static final int OUTTAKE_BUTTON = 5;
     public static final int FULL_SPEED_BUTTON = 11;
-    public static final int SHIFT_BUTTON = 7;
+    public static final int SHIFT_BUTTON = 0;
 }
