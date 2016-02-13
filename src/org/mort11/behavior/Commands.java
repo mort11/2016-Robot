c75b1cf17c1a235128a2e5584eb27210fcdd5036
Jakob Shortell <jshortell@mort11.org>
2016-02-12 20:41:10 -0500

    Added comments and commit #254!

--- src/org/mort11/behavior/Commands.java
+++ src/org/mort11/behavior/Commands.java
@@ -4,20 +4,27 @@ package org.mort11.behavior;
  * Commands - ToDo class description
  *
  * @author Matt Turi <mturi@mort11.org>
+ * @author Jakob Shortell <jshortell@mort11.org>
  */
+
+	
 public class Commands {
-    public enum HoodRequest {
+	// Positions for the hood
+    public enum HoodRequest { 
         NONE, POP, STOW, DISABLED
     }
 
+    // Motor state for roller
     public enum RollerRequest {
         NONE, INTAKE, EXHAUST, DISABLED
     }
 
+    // Preset angles for the Arm
     public enum IntakeArmPresets {
         NONE, STORED, DOWN, GROUND, MANUAL_CTRL, DISABLED
     }
 
+    // Motor state for the fly wheel
     public enum FlywheelRequest {
         NONE, STOP, START, DISABLED
     }
