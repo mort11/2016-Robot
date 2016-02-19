2da7401c7ab8791da9eba459cd9107928c8d1408
Matt Turi <mturi@mort11.org>
2016-02-18 21:34:22 -0500

    Probably fixed intake woes

--- src/org/mort11/util/powermanager/MORTCANTalon.java
+++ src/org/mort11/util/powermanager/MORTCANTalon.java
@@ -2,28 +2,30 @@ package org.mort11.util.powermanager;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import org.mort11.util.MORTSubsystem;
 
 /**
  * MORTCANTalon - Implementation of CANTalon with ability to monitor current draw
  *
  * @author Matt Turi <mturi@mort11.org>
  */
-public class MORTCANTalon extends CANTalon {
+public class MORTCANTalon extends CANTalon implements MORTSubsystem {
+    public String name;
     private PowerDistributionPanel pdp = new PowerDistributionPanel();
     private int pdpSlot;
     private boolean disabled = false;
-    private boolean reverse;
 
     /**
      * Creates a CANTalon that controls a single motor
      *
      * @param deviceNumber CAN ID for talon
-     * @param reverse      Reverse motor
+     * @param pdpSlot      int of PDP slot motor is connected to
      */
-    public MORTCANTalon(int deviceNumber, int pdpSlot, boolean reverse) {
+    public MORTCANTalon(int deviceNumber, int pdpSlot, String subsystemName) {
         super(deviceNumber);
         this.pdpSlot = pdpSlot;
-        this.reverse = reverse;
+        this.name = subsystemName;
+        System.out.println(String.format("Created new MORTCANTalon [%s] [ID-%s]", name, String.valueOf(super.getDeviceID())));
     }
 
     /**
@@ -31,40 +33,43 @@ public class MORTCANTalon extends CANTalon {
      *
      * @param speed Motor speed
      */
+    @Override
     public void set(double speed) {
-        if (this.disabled) {
-            super.set(0);
-        } else {
-            super.set(this.reverse ? speed * -1 : speed);
-        }
+        super.set(speed);
     }
 
+    /**
+     * @return Current of given PDP channel
+     */
     public double getCurrent() {
-        return this.pdp.getCurrent(pdpSlot);
+        return pdp.getCurrent(pdpSlot);
     }
 
-//    /**
-//     * Check if subsystem is disabled
-//     *
-//     * @return Subsystem state
-//     */
-//    public boolean isDisabled() {
-//        return this.disabled;
-//    }
-//
-//    /**
-//     * Disable the subsystem
-//     */
-//    public void disable() {
-//        this.disabled = true;
-//    }
-//
-//    /**
-//     * Re-enable subsystem that is in a disabled state
-//     */
-//    public void enable() {
-//        this.disabled = false;
-//    }
+    /**
+     * Check if subsystem is disabled
+     *
+     * @return Subsystem state
+     */
+    @Override
+    public boolean isDisabled() {
+        return this.disabled;
+    }
+
+    /**
+     * Disable the subsystem
+     */
+    @Override
+    public void disable() {
+        this.disabled = true;
+    }
+
+    /**
+     * Re-enable subsystem that is in a disabled state
+     */
+    @Override
+    public void enable() {
+        this.disabled = false;
+    }
 
     /**
      * Get voltage
