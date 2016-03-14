3884511afe0576dc77787c19022b341f68ade959
sahit <chsahit@gmail.com>
2016-03-14 19:35:01 -0400

    lined up according to the window we found

--- src/org/mort11/commands/auton/AdjustToGoal.java
+++ src/org/mort11/commands/auton/AdjustToGoal.java
@@ -16,20 +16,18 @@ import org.mort11.util.PIDLoop;
  */
 public class AdjustToGoal extends Command {
     private boolean isFinished = false;
-    private DTSide left = Robot.adaptor.leftSide;
+    private DTSide left = Robot.adaptor.leftSide;	
     private DTSide right = Robot.adaptor.rightSide;
     private double area = 0;
     private int curr_index = 0,target_index = 0;
     PIDLoop pid_turn;
+    private double angle,range,pxPer6,x_val,theta;
     public AdjustToGoal() {
         requires(left);
         requires(right);
         pid_turn = new PIDLoop(180, 0.01, 0,15);
     }
     protected void initialize() {
-        
-    }
-    protected void execute() {    	
         if (Robot.table.getNumberArray("centerX", new double[]{}).length == 0) {
             System.out.println("Not found in ATG");
             this.isFinished = true;
@@ -42,7 +40,15 @@ public class AdjustToGoal extends Command {
         		target_index = curr_index;
         	}
         	curr_index++;
-        }        
+        }
+        x_val =Robot.table.getNumberArray("centerX",new double[]{})[target_index];
+        double y_val =Robot.table.getNumberArray("centerY",new double[]{})[target_index];
+        range = (y_val + 68.05)/1.475;
+        pxPer6 = -0.7083*range+ 104.75;
+        theta = Math.atan2((((153 - x_val)/pxPer6) * 6),range) * 180/Math.PI;
+        System.out.println("will turn: " + theta);
+    }
+    protected void execute() {    	        
         System.out.println("Centering"); 
 //        if (Robot.table.getNumberArray("centerX", new double[]{})[target_index] < 158) {
 //            this.left.set(-0.25);
@@ -56,10 +62,12 @@ public class AdjustToGoal extends Command {
 //            System.out.println("Centered");
 //            this.isFinished = true;
 //        }
-        left.set(-pid_turn.getOutput(
-        		Robot.table.getNumberArray("centerX", new double[]{})[target_index]));
-        right.set(pid_turn.getOutput(
-        		Robot.table.getNumberArray("centerX", new double[]{})[target_index]));
+//        left.set(-pid_turn.getOutput(
+//        		Robot.table.getNumberArray("centerX", new double[]{})[target_index]));
+//        right.set(pid_turn.getOutput(
+//        		Robot.table.getNumberArray("centerX", new double[]{})[target_index]));
+        
+        System.out.println("current angle");
     }
     protected boolean isFinished() {
         return Math.abs(pid_turn.getOutput(
